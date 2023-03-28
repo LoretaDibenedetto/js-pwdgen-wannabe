701f94ea69df3e5a12b16b4ae83ac064cd672ee9
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-03-28 14:57:50 +0200

    add comment

--- script.js
+++ script.js
@@ -6,23 +6,33 @@ const nome = prompt("inserisci il tuo nome:");
 const lastName = prompt("inserisci il tuo cognome:");
 let preferColor = prompt("inserisci il tuo colore preferito:")
 
+//salvo un numero in una variabile
 let staticNum = 21;
 
-let num1 = prompt("inserisci il primo numero:");
-let num2 = prompt("inserisci il secondo numero:");
+let num1 = prompt("inserisci la prima cifra:");
 
+//rendo questa stringa un numero intero
+num1 = parseInt(num1);
+
+let num2 = prompt("inserisci la seconda cifra:");
+
+//rendo questa stringa un numero intero
+num2 = parseInt(num2);
+
+//divido i due valori presi dai prompt
 let div = num1 / num2; 
 
-let visitCard = `
-----------------------------------------------------
-nome: ${nome},
-cognome: ${lastName},
-colore preferito: ${preferColor}, 
-numero ${staticNum}, 
 
-se fai una divisione tra ${num1} e ${num2} il risultato e': ${div}
------------------------------------------------------
+//racchiudo in una variabile il template literal
+let passwordGenerator = `
+
+ ${nome}${lastName}${preferColor}${staticNum}${div}
+
 
 `;
 
-console.log(visitCard);
+console.log(passwordGenerator)
+
+//aggiungo nell html
+document.getElementById("password").innerText = passwordGenerator;
+
